63915a93eb55c1c78cf9b32cd1b4bd09f1e25124
Hao Dong <dong_hao@hotmail.co.uk>
2018-06-10 22:08:11 +0100

    Formatted files

--- rollup.config.js
+++ rollup.config.js
@@ -4,15 +4,17 @@ import resolve from 'rollup-plugin-node-resolve';
 // import uglify from 'rollup-plugin-uglify';
 
 export default {
-  input: 'src/serina.ts',
-  output: [{
-    name: 'Serina',
-    file: 'dist/serina.js',
-    format: 'umd'
-  }],
-  plugins: [
-    typescript(),
-    resolve()
-    // commonjs()
-  ]
-}
+    input: 'src/serina.ts',
+    output: [
+        {
+            name: 'Serina',
+            file: 'dist/serina.js',
+            format: 'umd'
+        }
+    ],
+    plugins: [
+        typescript(),
+        resolve()
+        // commonjs()
+    ]
+};
