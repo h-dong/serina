257f131c9e0c2a128afc358b3c61e22c7d03460a
Hao Dong <1476807+h-dong@users.noreply.github.com>
2019-06-29 17:05:36 +0100

    Fix case if docs directory doesn't exist

--- scripts/build-doc.sh
+++ scripts/build-doc.sh
@@ -2,9 +2,10 @@
 
 # clean up docs folder
 rm -rf docs/*
+mkdir -p docs/
 
 # copy over html file
-cp public/index.html docs
+cp public/index.html docs/
 
 # copy over library
-cp dist/serina.js docs
+cp dist/serina.js docs/
