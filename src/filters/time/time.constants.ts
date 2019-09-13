071404f52860a1c23d50b99a3585efb89cee87d1
Motiejus Gudenas <mj.gudenas@gmail.com>
2019-09-13 19:10:18 +0100

    Unmaking optional meridiem

--- src/filters/time/time.constants.ts
+++ src/filters/time/time.constants.ts
@@ -4,7 +4,7 @@ const MERIDIEM = `( )?(${AM}|${PM})`;
 const FILLER_WORDS = '(at|by) ';
 
 const TIME = {
-    ANY: `(${FILLER_WORDS})?[0-9]{1,2}(((:[0-5][0-9])(${MERIDIEM}?))|(:[0-5][0-9])|(${MERIDIEM}?))`,
+    ANY: `(${FILLER_WORDS})?[0-9]{1,2}(((:[0-5][0-9])(${MERIDIEM}))|(:[0-5][0-9])|(${MERIDIEM}))`,
     FILLER_WORDS,
 };
 
