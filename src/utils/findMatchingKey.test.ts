1e4a56a9c47c4f591cfe6311fb0a8158de1241ef
Motiejus Gudenas <mj.gudenas@gmail.com>
2019-09-25 18:09:05 +0100

    Feature/relative days (#32)
    * Quentin Tarlintino
    
    * Updated readme
    
    * Removing redundant comment
    
    * Adding to gitignore
    
    * Simplify logic to get rid of nulls, add a utils function for looping

---
+++ src/utils/findMatchingKey.test.ts
@@ -0,0 +1,25 @@
+import findMatchingKey from './findMatchingKey';
+
+describe('findMatchingKey()', () => {
+    const testObject = {
+        first: 1,
+        second: 2,
+        third: 3,
+    };
+
+    test('should return null when no match is found', () => {
+        const check = findMatchingKey(testObject, '[4]');
+        expect(check).toBe(null);
+    });
+
+    test('should return a matching key when a match is found', () => {
+        const check = findMatchingKey(testObject, '[3]');
+        expect(check).toBe('third');
+    });
+
+    test('should return the first match when multiple are found', () => {
+        const check = findMatchingKey(testObject, '[1-3]');
+        expect(check).toBe('first');
+    });
+
+});
