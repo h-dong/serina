efd5ba8603bf109da2dd4ac1ee3503901ce92063
Hao Dong <dong_hao@hotmail.co.uk>
2019-09-28 03:49:18 +0100

    Updated how to match against time

--- src/utils/convertTimeStringToObj.ts
+++ src/utils/convertTimeStringToObj.ts
@@ -1,36 +1,50 @@
 import { TimeObjectSchema } from 'serina.schema';
-import { MERIDIEM, PM } from 'filters/time/time.constants';
+import TIME from 'filters/time/time.constants';
 import contains from './contains';
-import { remove, isValidTime } from 'utils';
+import { matchPattern } from 'utils';
 
-function convertTimeStringToObj(time: string): TimeObjectSchema {
+function getValidMatch(text: string, pattern: string): string {
+    const matched = matchPattern(text, pattern, false);
+    if (!matched || matched.length === 0) return null;
+    const [ value ] = matched;
+    return value;
+}
+
+function convertTimeStringToObj(timeString: string): TimeObjectSchema {
     let hour;
     let minute;
-
-    if (contains(time, `${MERIDIEM}`, false)) {
-        // 12H
-        const timeWithMeridiem = remove(time, `${MERIDIEM}`, false);
-        const [ hourStr, minStr ] = timeWithMeridiem.split(':');
-        hour = parseInt(hourStr, 10);
-        minute = parseInt(minStr, 10);
-
-        if (isNaN(minute)) minute = 0;
-
-        if (contains(time, `${PM}`,false)) {
-            // Post Meridiem (PM)
-            if (hour < 12) hour += 12;
-        } else {
-            // Ante Meridiem (AM)
-            if (hour === 12) hour = 0;
+    
+    const isRelativeTime = contains(timeString, TIME.RELATIVE_TIME_FILLER_WORDS);
+    
+    if (isRelativeTime) {
+        if (contains(timeString, TIME.VERBAL_QUANTIFIERS)) { // half or quarter
+            hour = getValidMatch(timeString, `(?<=${TIME.RELATIVE_TIME_FILLER_WORDS}( ))${TIME.HOUR_PART}`)
+            if (contains(timeString, TIME.HALF)) minute = '30';
+            if (contains(timeString, TIME.QUARTER)) minute = '15';
+        } else { // 20 min to 7pm
+            hour = getValidMatch(timeString, `(?<=${TIME.RELATIVE_TIME_FILLER_WORDS}( ))${TIME.HOUR_PART}(?=((( )${TIME.MERIDIEM}))?)`);
+            minute = getValidMatch(timeString, `\\b${TIME.MINUTE_PART}(?=(( )(${TIME.MINUTE_IDENTIFIER}( ))?${TIME.RELATIVE_TIME_FILLER_WORDS}))`);
         }
     } else {
-        // 24H
-        const [ hourStr, minStr ] = time.split(':');
-        hour = parseInt(hourStr, 10);
-        minute = parseInt(minStr, 10);
+        if (contains(timeString, TIME.DIVIDER)) { // 19:09
+            hour = getValidMatch(timeString, `\\b${TIME.HOUR_PART}(?=${TIME.DIVIDER})`);
+            minute = getValidMatch(timeString, `(?<=${TIME.DIVIDER})${TIME.MINUTE_PART}`);
+        } else { // 6pm
+            hour = getValidMatch(timeString, `\\b${TIME.HOUR_PART}(?=(( )?${TIME.MERIDIEM}))`);
+            minute = 0;
+        }
     }
 
-    return isValidTime(hour, minute) ? { hour, minute } : null;
+    hour = parseInt(hour, 10);
+    minute = parseInt(minute, 10);
+    if (isNaN(hour) || isNaN(minute) || hour > 23 || minute > 59) return null;
+    if (contains(timeString, TIME.AM, false) && hour === 12) hour = 24;
+    if (contains(timeString, TIME.TO) && hour > 0) hour -= 1;
+    if (contains(timeString, TIME.TO)) minute = 60 - minute;
+    if (contains(timeString, TIME.PM, false) && hour < 12) hour += 12;
+    if (hour === 24) hour = 0;
+
+    return { hour, minute };
 }
 
 export default convertTimeStringToObj;
