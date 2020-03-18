8f6c1bb33378e74da134fe9622c6fcb89d89aaae
Hao Dong <dong_hao@hotmail.co.uk>
2020-03-18 01:15:42 +0000

    Bug Fix: Safari does not support regex lookbehind

--- src/utils/convertTimeStringToObj.ts
+++ src/utils/convertTimeStringToObj.ts
@@ -6,7 +6,7 @@ import matchPattern from 'utils/matchPattern';
 function getValidMatch(text: string, pattern: string): string {
     const matched = matchPattern(text, pattern, false);
     if (!matched || matched.length === 0) return null;
-    const [ value ] = matched;
+    const [value] = matched;
     return value;
 }
 
@@ -16,19 +16,54 @@ function convertTimeStringToObj(timeString: string): TimeObjectSchema {
     const isRelativeTime = contains(timeString, TIME.RELATIVE_TIME_FILLER_WORDS);
 
     if (isRelativeTime) {
-        if (contains(timeString, TIME.VERBAL_QUANTIFIERS)) { // half or quarter
-            hour = getValidMatch(timeString, `(?<=${TIME.RELATIVE_TIME_FILLER_WORDS}( ))${TIME.HOUR_PART}`);
+        if (contains(timeString, TIME.VERBAL_QUANTIFIERS)) {
+            // half or quarter
+            try {
+                hour = getValidMatch(timeString, `(?<=${TIME.RELATIVE_TIME_FILLER_WORDS}( ))${TIME.HOUR_PART}`);
+            } catch {
+                // workaround for browsers that doesn't support regex lookbehind
+                const hourWithFillerWords = getValidMatch(
+                    timeString,
+                    `(${TIME.RELATIVE_TIME_FILLER_WORDS}( ))${TIME.HOUR_PART}`
+                );
+                hour = getValidMatch(hourWithFillerWords, TIME.HOUR_PART);
+            }
+
             if (contains(timeString, TIME.HALF)) minute = '30';
             if (contains(timeString, TIME.QUARTER)) minute = '15';
-        } else { // 20 min to 7pm
-            hour = getValidMatch(timeString, `(?<=${TIME.RELATIVE_TIME_FILLER_WORDS}( ))${TIME.HOUR_PART}(?=((( )${TIME.MERIDIEM}))?)`);
-            minute = getValidMatch(timeString, `\\b${TIME.MINUTE_PART}(?=(( )(${TIME.MINUTE_IDENTIFIER}( ))?${TIME.RELATIVE_TIME_FILLER_WORDS}))`);
+        } else {
+            // 20 min to 7pm
+            try {
+                hour = getValidMatch(
+                    timeString,
+                    `(?<=${TIME.RELATIVE_TIME_FILLER_WORDS}( ))${TIME.HOUR_PART}(?=((( )${TIME.MERIDIEM}))?)`
+                );
+            } catch {
+                // workaround for browsers that doesn't support regex lookbehind
+                const hourWithFillerWords = getValidMatch(
+                    timeString,
+                    `(${TIME.RELATIVE_TIME_FILLER_WORDS}( ))${TIME.HOUR_PART}(?=((( )${TIME.MERIDIEM}))?)`
+                );
+                hour = getValidMatch(hourWithFillerWords, TIME.HOUR_PART);
+            }
+            minute = getValidMatch(
+                timeString,
+                `\\b${TIME.MINUTE_PART}(?=(( )(${TIME.MINUTE_IDENTIFIER}( ))?${TIME.RELATIVE_TIME_FILLER_WORDS}))`
+            );
         }
     } else {
-        if (contains(timeString, TIME.DIVIDER)) { // 19:09
+        if (contains(timeString, TIME.DIVIDER)) {
+            // 19:09
             hour = getValidMatch(timeString, `\\b${TIME.HOUR_PART}(?=${TIME.DIVIDER})`);
-            minute = getValidMatch(timeString, `(?<=${TIME.DIVIDER})${TIME.MINUTE_PART}`);
-        } else { // 6pm
+            try {
+                minute = getValidMatch(timeString, `(?<=${TIME.DIVIDER})${TIME.MINUTE_PART}`);
+            } catch {
+                // workaround for browsers that doesn't support regex lookbehind
+                const minWithDivider = getValidMatch(timeString, `(${TIME.DIVIDER})${TIME.MINUTE_PART}`);
+                minute = getValidMatch(minWithDivider, TIME.MINUTE_PART);
+            }
+        } else {
+            // 6pm
             hour = getValidMatch(timeString, `\\b${TIME.HOUR_PART}(?=(( )?${TIME.MERIDIEM}))`);
             minute = 0;
         }
