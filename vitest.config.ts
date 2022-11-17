00a3b700a71f9aabf2202ff3186b7858270a119d
Hao Dong <hey@hao.dev>
2022-11-17 01:55:48 +0000

    Add filter to vitest absolute path

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
         alias: [
             { find: 'serina.schema', replacement: path.resolve(__dirname, 'src', 'serina.schema') },
             { find: 'utils', replacement: path.resolve(__dirname, 'src', 'utils') },
+            { find: 'filters', replacement: path.resolve(__dirname, 'src', 'filters') },
             { find: 'lib', replacement: path.resolve(__dirname, 'src', 'lib') },
         ],
     },
