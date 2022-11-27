eb63ca043773ada368d86d112a336e42210907cc
Hao Dong <hey@hao.dev>
2022-11-27 03:30:46 +0000

    Update test script to generate coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         globals: true,
+        coverage: {
+            provider: 'istanbul',
+        },
     },
     resolve: {
         alias: [
