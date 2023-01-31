37c35753be20e44bc00bd571b983dcd0c72ea035
Hao Dong <hey@hao.dev>
2023-01-31 02:04:01 +0000

    Remove unnecessary tests

--- src/filters/day/day.test.ts
+++ src/filters/day/day.test.ts
@@ -1,5 +1,5 @@
 import Day from './day';
-import { ParsedMatchSchema } from 'serina.schema';
+
 describe('Day', () => {
     describe('Normal behaviour', () => {
         beforeAll(() => {
@@ -29,49 +29,6 @@ describe('Day', () => {
                     text: 'test string',
                 },
             ]);
-        });
-    });
-
-    describe('Edge Cases (Integration)', () => {
-        const text = 'go to library';
-
-        beforeAll(() => {
-            // Mock Date Time to Saturday, 19 February 2019 18:06:18 GMT+00:00
-            vi.useFakeTimers();
-            vi.setSystemTime(new Date(Date.UTC(2019, 2, 19)));
-        });
-
-        afterAll(() => {
-            vi.useRealTimers();
-        });
-
-        test('should not parse any date before 1st', () => {
-            const filter = new Day();
-            expect(filter.parseText(`${text} 0th`)).toBe(null);
-        });
-
-        test('should not parse a single number', () => {
-            const filter = new Day();
-            expect(filter.parseText('buy milk 20')).toBe(null);
-        });
-
-        test('should not parse any dates beyond 31st', () => {
-            const filter = new Day();
-            expect(filter.parseText(`${text} 32nd`)).toBe(null);
-        });
-
-        test('should skip months until a month that has it', () => {
-            // e.g. if 31st is asked during Feb, we should skip to March (next month with that date)
-            const date = '31st';
-            const result: ParsedMatchSchema[] = [
-                {
-                    dateTime: new Date('2019-03-31T00:00:00.000Z'),
-                    text,
-                    matched: date,
-                },
-            ];
-            const filter = new Day();
-            expect(filter.parseText(`${text} ${date}`)).toEqual(result);
         });
     });
 });
